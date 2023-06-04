ae13a7bc0b7190b25d1855e7fcba270e3ea4db42
John Chidiac <77592769+ioennes@users.noreply.github.com>
2023-06-04 15:46:30 +0300

    Add files via upload

---
+++ java/Sorting/SelectionSort.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+public class SelectionSort {
+
+  static void sSort(int[] array) {
+    for(int i = 0; i < array.length; i++) {
+      int min = i;
+      for(int j = i + 1; j < array.length; j++) {
+        if(array[min] > array[j]) min = j;
+      }
+      int temp = array[min];
+      array[min] = array[i];
+      array[i] = temp;
+    }
+  }
+
+  static void rsSort(int[] array, int i) {
+    if(i == array.length) return;
+    int min = i;
+    for(int j = i + 1; j < array.length; j++) {
+      if(array[min] > array[j]) min = j;
+    }
+    int temp = array[min];
+    array[min] = array[i];
+    array[i] = temp;
+
+    rsSort(array, i + 1);
+  }
+}
